abf7ec61f8af12903fc4cfcd6658f3a7206fca3d
Swarm Coders <seajure@googlegroups.com>
2012-03-17 14:47:52 -0700

    Initial commit.

---
+++ src/swarm_project/core.clj
@@ -0,0 +1,6 @@
+(ns swarm-project.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/swarm_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns swarm-project.core-test
+  (:use clojure.test
+        swarm-project.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
