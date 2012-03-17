1d5dfa609c3288ac7040bacb38f5440a22f31c99
Swarm Coders <seajure@googlegroups.com>
2012-03-17 15:59:09 -0700

    Our test passes.

--- src/swarm_project/core.clj
+++ src/swarm_project/core.clj
@@ -1,6 +1,30 @@
-(ns swarm-project.core)
+(ns swarm-project.core
+  (:use [clojure.set :only (difference)])
+  (:use [clojure.test]))
+
+(def blacklist #{:clojure :clj-config})
+
+(with-test 
+  (defn is-a-root? [node]
+    (let [deps (difference (get-in node [:name :dependencies]) blacklist)] 
+      (empty? deps)))
+  (is (is-a-root? {:name {:dependencies blacklist}}))
+  (is (is-a-root? {:name {:dependencies #{}}})))  
+  (is (not (is-a-root? {:name {:dependencies #{:something}}})))   
+
+
+(defn find-roots
+  [graph]
+  (filter is-a-root? graph))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  ;; Read in the file
+  ;; Find roots of the graph
+  ;;  ???
+  ;; Profit
+  (let [graph (read-string (slurp "./melange"))
+        roots (find-roots graph)] 
+  (println roots)))
+
